35dae4e0729ed38f47b3e70142ded9bd3f59d203
Tifider <Tifider@gmail.com>
2018-09-18 23:39:51 +0300

    Add some JavaDocks and package

--- src/task01/traffic/light/TrafficLight.java
+++ src/task01/traffic/light/TrafficLight.java
@@ -1,9 +1,20 @@
+package task01.traffic.light;
 import java.util.Scanner;
-
-public class Loader {
+/**
+ * Traffic light task for Epam Java Development Courses
+ * Gets minute as input and returns color of current signal
+ * @author Selin Roman
+ * @version 0.1
+ */
+public class TrafficLight {
     
     public static void main(String[] args) {
-        
+    
+        /*
+         * Here is data area
+         * time is what user inputs
+         * cycleDuration represents full cycle of signals
+        */
         int time;
         int redDuration = 2;
         int yellowDuration = 3;
@@ -13,10 +24,12 @@ public class Loader {
         System.out.println("Enter time in minutes ");
         Scanner scanner = new Scanner(System.in);
         time = scanner.nextInt();
-        
+    
+        /*
+         * First we find out at what minute of cycle we're now
+         * Then we check which signal traffic light shows
+        */
         time %= cycleDuration;
-        System.out.println(time);
-        
         if (time <= redDuration) {
             System.out.println("Red");
         } else if (time <= redDuration + yellowDuration) {
